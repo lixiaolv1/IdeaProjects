2958b3e140fc271ef5caac63709ee3878456080b
lixiaolv1 <48149679+lixiaolv1@users.noreply.github.com>
2019-04-13 18:25:17 +0800

    4-13更新

--- Test/src/StringDemo.java
+++ Test/src/StringDemo.java
@@ -1,6 +1,30 @@
+import java.io.UnsupportedEncodingException;
+
 public class StringDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         //字符串的不可变性
+        //演示常用的几种String对象的构建方式
+        String str1 = "你好世界！";
+        String  str2 = new String();//this.value = "".value  此方法是新建一个空间然后将空字符串赋值给新的空间
+        String str3 = new String(str1);//this.value = original.value;this.hash = original.hash; 将值和hash值同事赋给新对象空间
+        System.out.println(str1 == str3);//但他们不相等
+
+        char[] chArray = {'a','b','c'};//在C中，字符数组和字符串的区别就在于最后一个元素是否是\0
+        //将字符数组转换成数组
+        String str4 = new String(chArray);
+        //当然也可以将字符串转回字符数组
+        chArray = str4.toCharArray();
+        System.out.println(str4);
+
+        //字节数组和字符串的转换 byte数据类型：-128到127之间
+        byte[] byteArray = {104,101,108,108,111};
+        String str5 = new String(byteArray);
+        System.out.println("转义结果为" + str5);
+
+        //字符串转码
+        String str6 = new String(str1.getBytes(),"UTF-8");
+        System.out.println(str6);
+
 
         String value = "德玛西亚之力";
         //String到底是什么？  实际上是char型数组的包装类
@@ -22,6 +46,7 @@ public class StringDemo {
         //参考最后步骤，直接成功--有小毛病，为什么呢，是因为没有写更新备注吗-为什么啊
         //难道是文件和日志必须要同时更新？？？
         //4-13 00:33临睡前更新，慢慢来吧
+        //aaaaa,还特么老代码覆盖了新代码
     }
 }
 
