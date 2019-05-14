e63f19ed866d620fedabd990ba9d5a96f85f7f71
lixiaolv1 <48149679+lixiaolv1@users.noreply.github.com>
2019-05-14 18:31:21 +0800

    5月14日更新

--- Test/src/Calendar/CalendarDemo.java
+++ Test/src/Calendar/CalendarDemo.java
@@ -4,7 +4,11 @@ package Calendar;
  */
 
 
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.SimpleTimeZone;
 
 public class CalendarDemo {
     public static void main(String[] args) {
@@ -22,11 +26,21 @@ public class CalendarDemo {
         int milliSencond = cal.get( Calendar.MILLISECOND );
         System.out.println("当前时间：");
 
-        String strTime = String.format( "%d-%02d-%02d %d-%d-%d:%s" ,
+        String strTime = String.format( "%d-%02d-%02d  %d:%d:%d_%s" ,
                 year, month, day, hour, minute, sencond, milliSencond
                 );
         System.out.println(strTime);
 
-    }
+        //为日历对象设定时间
+        cal.set( 2012,2,5,14,28,4 );
+        Date date = cal.getTime();
+        System.out.println( date.toLocaleString());
 
+        // 日期格式化类
+        SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss:SSS" );
+        String strDate = format.format( date );
+        System.out.println(strDate);
+
+
+    }
 }
