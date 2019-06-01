7eebae5012556caafd2c050c5553693da3e0c9a1
lixiaolv1 <48149679+lixiaolv1@users.noreply.github.com>
2019-06-01 18:31:46 +0800

    6/01

--- Test/src/ModelDemo1/HeroTest.java
+++ Test/src/ModelDemo1/HeroTest.java
@@ -5,11 +5,13 @@ public class HeroTest {
         //两个战士之间的对战
         Hero hero1 = new Warrior( 1,"盖伦",0,0 );
         Hero hero2 = new Warrior( 2,"貂蝉",500,50 );
-        hero1.PK( hero2 );
+ //       hero1.PK( hero2 );
 
         Hero hero3 = new Magical( 3,"曹操",500,5, 15 );
-//        hero3.fight( hero1 );
-//        hero1.fight( hero3 );
+        hero3.PK( hero1 );
+        hero1.PK( hero3 );
+
+
 
     }
 
