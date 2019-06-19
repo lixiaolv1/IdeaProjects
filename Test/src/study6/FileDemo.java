ac6028df91e47fde021eb4c76ee97cc2c3fce36d
lixiaolv1 <48149679+lixiaolv1@users.noreply.github.com>
2019-06-19 18:43:24 +0800

    6-19更新

--- Test/src/study6/FileDemo.java
+++ Test/src/study6/FileDemo.java
@@ -1,23 +1,24 @@
 package study6;
-/*
- * 演示File类的常用方法
- */
 
 import java.io.File;
 
 public class FileDemo {
 
-    //当前要操作的文件路劲常量
-    private static final String FilePath = "src/study6/FileDemo.java";
+    //使用常量来定义路径方便修改
+    //切记删除文件的时候慎用，刚才就踏马把这个java文件删了
+    private static final String FilePath = "Test/src/study6/FileDemo.java";
+    //在Windows平台下，路径可以写作 “Test\\src\\study6\\FileDemo.java”,但是不建议这么做
+
     public static void main(String[] args) {
-        //new File(".");点就表示当前根目录文件
-        File file = new File("FilePath");
-        System.out.println("文件/文件夹是否存在:" + file.exists());
-        System.out.println("是不是一个文件：" + file.isFile());
-        System.out.println("是不是一个文件夹：" +file.isDirectory());
-        System.out.println("文件/目录名：" + file.getName());
-        System.out.println("文件的绝对路径是" + file.getAbsolutePath());
-        System.out.println("路径：" + file.getPath());
+        //“.”路径表示当前工程根目录
+        File file = new File(FilePath);
+
+        System.out.println("文件/文件夹是否存在： " + file.exists());
+        System.out.println("是不是一个文件： " + file.isFile());
+        System.out.println("是不是一个文件夹： " + file.isDirectory());
+        System.out.println("文件名/目录名： " + file.getName());
+        System.out.println("绝对路径：" + file.getAbsolutePath());
+        System.out.println("路径： " + file.getPath());
 
 
     }
